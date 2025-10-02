382ba6bbfb8ecb0d23d901194ad0fe0d9a273593
AlejandroMtz07 <alejandro23juanito@gmail.com>
2025-10-02 16:59:20 -0700

    feat(Login): initial parts for the login view

--- src/Views/Login/Login.tsx
+++ src/Views/Login/Login.tsx
@@ -1,8 +1,36 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+//Definition of the user schema
+const UserSchema = z.object({
+  email: z.email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+//User type for get the data in the onSubmit function
+type User = z.infer<typeof UserSchema>;
 
 export default function Login() {
+
+  const {register, handleSubmit, formState: {errors}} = useForm({
+    resolver: zodResolver(UserSchema)
+  });
+  const onSubmit = (data:User)=>{
+    console.log(data);
+  }
+
   return (
     <div>
-        Contend
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('email', {required: true})}/><br/>
+        {errors.email && <p style={{color: "red"}}>{errors.email.message}</p>}
+        <input {...register('password',{required: true})}/><br/>
+        {errors.password && <p style={{color: "red"}}>{errors.password.message}</p>}
+        <button type="submit">
+          Login
+        </button>
+      </form>
     </div>
   )
 }
