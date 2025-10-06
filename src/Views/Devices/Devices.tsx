8d78f7dd304001451851ddea2dd6d8a992c757bc
AlejandroMtz07 <alejandro23juanito@gmail.com>
2025-10-05 21:42:19 -0700

    Simple device showing for tests

--- src/Views/Devices/Devices.tsx
+++ src/Views/Devices/Devices.tsx
@@ -1,12 +1,38 @@
+import axios from 'axios';
 import style from './Devices.module.css';
+import { useEffect, useState } from 'react';
+
+type Device = {
+  id: number,
+  lastlecture: Date,
+  lastdata: string,
+}
 
 export default function Devices() {
 
-  //TODO Fetch devices already registered from the user
+  const [devices, setDevices] = useState([]);
+
+  useEffect(()=>{
+    axios.get(
+      'http://localhost:8080/devices',
+      {headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}}
+    ).then((result)=>{
+      console.log(result);
+      setDevices(result.data);
+    }).catch((error)=>{
+      console.log(error);
+    })
+  },[])
   
   return (
     <div className={style.devices}>
-      Devices
+      {devices.map((item:Device,index)=>(
+        <ul key={item.id} className={style.single_device}>
+          <li>{index}</li>
+          <li>{item.lastlecture.toString()}</li>
+          <li>{item.lastdata}</li>
+        </ul>
+      ))}
     </div>
   )
 }
