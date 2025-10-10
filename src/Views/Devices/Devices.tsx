438828869c721e68e8e88546c23d926045c0f792
AlejandroMtz07 <alejandro23juanito@gmail.com>
2025-10-09 18:30:07 -0700

    Some visual changes.

--- src/Views/Devices/Devices.tsx
+++ src/Views/Devices/Devices.tsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import style from './Devices.module.css';
 import { useEffect, useState } from 'react';
+import { FiThermometer } from 'react-icons/fi';
 
 type Device = {
   id: number,
   status: number,
-  lastlecture: Date,
+  lastlecture: string,
   lastdata: string,
 }
 
@@ -13,28 +14,39 @@ export default function Devices() {
 
   const [devices, setDevices] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get(
       'http://localhost:8080/devices',
-      {headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}}
-    ).then((result)=>{
+      { headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` } }
+    ).then((result) => {
       console.log(result);
       setDevices(result.data);
-    }).catch((error)=>{
-      console.log(error);
+    }).catch((error) => {
+      console.log({ msg: error });
     })
-  },[])
-  
+  }, [])
+
   return (
     <div className={style.devices}>
-      {devices.map((item:Device,index)=>(
-        <ul key={index} className={style.single_device}>
-          <li>{item.id}</li>
-          <li>Status: {(item.status===0)?'Inactive':'Active'}</li>
-          <li>Last lecture: {item.lastlecture.toString()}</li>
-          <li>Last data: {item.lastdata}</li>
-        </ul>
-      ))}
+      {devices.map((item: Device, index) => {
+        const date = new Date(item.lastlecture);
+        const formattedDate = new Intl.DateTimeFormat("es-MX", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          timeZone: "America/Mazatlan", // ajusta según tu zona
+          hour12: false,
+        }).format(date);
+
+        return (
+          <div key={index} className={style.single_device}>
+            <div className={style.date_device}>{formattedDate}</div>
+            <div className={style.lecture_device}>{item.lastdata} C°</div>
+          </div>
+        )
+      })}
     </div>
   )
 }
